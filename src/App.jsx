73df73d2c8cf367b58cff01331c127ee17cf8ec1
cruzayye <johnnyc010392@gmail.com>
2025-04-16 21:36:01 -0700

    updatd app js to render pics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { createRoot } from "react-dom/client";
 import Home from "./Home/Home";
 import { useState, useEffect } from "react";
-
+// import burritosImage from './Assets/burritos.jpg';
+// import tortasImage from './Assets/tortas.jpg';
+// import platesImage from './Assets/plates.jpg';
+import quesadillasImage from "./Assets/quesadillas.png";
+// import specialtiesImage from './Assets/specialties.jpg';
 import "./App.scss";
 import plateData from "./data/plates.json";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Plate from "./Plate/Plate";
+
+const imageMap = {
+  // BURRITOS: burritosImage,
+  // TORTAS: tortasImage,
+  // PLATES: platesImage,
+  QUESADILLAS: quesadillasImage,
+  // SPECIALTIES: specialtiesImage,
+};
 
 const App = () => {
   const [plates, setPlates] = useState([]);
@@ -25,7 +36,7 @@ const App = () => {
             name={plate.name}
             headline={plate.headline}
             description={plate.description}
-            image={plate.image}
+            image={imageMap[plate.name]}
           />
         }
       />
--- src/Plate/Plate.jsx
+++ src/Plate/Plate.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-import "./Plate.scss";
-import Sidebar from "../Sidebar/Sidebar";
+import React from 'react';
+import './Plate.scss';
+import Sidebar from '../Sidebar/Sidebar';
 
 const Plate = ({ name, headline, description, image }) => {
-  const getImageUrl = (relativePath) => {
-    return new URL(relativePath, import.meta.url).href;
-  };
+
 
   return (
-    <div className="plate">
+    <div className="plate" >
       <Sidebar />
       <div className="plate-content">
         <h2 className="plate-title">{name}</h2>
@@ -16,7 +14,7 @@ const Plate = ({ name, headline, description, image }) => {
         <p className="plate-description">{description}</p>
       </div>
       <div className="plate-image">
-        <img src={getImageUrl(image)} alt={name} />
+        <img src={image} alt={name} />
       </div>
     </div>
   );
