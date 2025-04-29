4c734f4ed308a3beab3ac0b09edb272cf6f0e0a7
cruzayye <johnnyc010392@gmail.com>
2025-04-29 07:37:41 -0700

    order now button

--- src/Plate/Plate.jsx
+++ src/Plate/Plate.jsx
@@ -1,18 +1,44 @@
-import React from 'react';
-import './Plate.scss';
-import Sidebar from '../Sidebar/Sidebar';
+import React from "react";
+import "./Plate.scss";
+import Sidebar from "../Sidebar/Sidebar";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+// Create a custom theme
+const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        outlined: {
+          borderColor: "#e3a45a",
+          color: "#e3a45a",
+          maxWidth: "fit-content",
+          "&:hover": {
+            borderColor: "#e3a45a",
+            backgroundColor: "rgba(227, 164, 90, 0.04)",
+          },
+        },
+      },
+    },
+  },
+});
 
 const Plate = ({ name, headline, description, image }) => {
-
-
   return (
-    <div className="plate" >
+    <div className="plate">
       <Sidebar />
       <div className="plate-content">
         <h2 className="plate-title">{name}</h2>
         <h3 className="plate-headline">{headline}</h3>
         <p className="plate-description">{description}</p>
       </div>
+
+      <div className="order-button">
+        <ThemeProvider theme={theme}>
+          <Button variant="outlined">ORDER NOW</Button>
+        </ThemeProvider>
+      </div>
+
       <div className="plate-image">
         <img src={image} alt={name} />
       </div>
